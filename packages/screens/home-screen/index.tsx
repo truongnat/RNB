23da30f6daa3858f49e6f0c4f65620466fde3fbd
Peanut <truongdq.dev@gmail.com>
2023-09-28 15:57:48 +0700

    feat: add coming soon component

--- packages/screens/home-screen/index.tsx
+++ packages/screens/home-screen/index.tsx
@@ -19,6 +19,34 @@ const HomeScreen = () => {
       <Drawer.Screen name="Typography" component={DemoTypo} />
       <Drawer.Screen name="Icon" component={DemoIcon} />
       <Drawer.Screen name="Avatar" component={DemoAvatar} />
+      <Drawer.Screen name="Form" component={DemoBox} />
+      <Drawer.Screen name="Pin Input" component={DemoBox} />
+      <Drawer.Screen name="Range" component={DemoBox} />
+      <Drawer.Screen name="Radio" component={DemoBox} />
+      <Drawer.Screen name="Checkbox" component={DemoBox} />
+      <Drawer.Screen name="Select" component={DemoBox} />
+      <Drawer.Screen name="Switch" component={DemoBox} />
+      <Drawer.Screen name="Spinner" component={DemoBox} />
+      <Drawer.Screen name="Dropdown" component={DemoBox} />
+      <Drawer.Screen name="Badge" component={DemoBox} />
+      <Drawer.Screen name="Card" component={DemoBox} />
+      <Drawer.Screen name="List" component={DemoBox} />
+      <Drawer.Screen name="Table" component={DemoBox} />
+      <Drawer.Screen name="Dialog" component={DemoBox} />
+      <Drawer.Screen name="Toast" component={DemoBox} />
+      <Drawer.Screen name="Tag" component={DemoBox} />
+      <Drawer.Screen name="Alert" component={DemoBox} />
+      <Drawer.Screen name="Circular Progress" component={DemoBox} />
+      <Drawer.Screen name="Process" component={DemoBox} />
+      <Drawer.Screen name="Skeleton" component={DemoBox} />
+      <Drawer.Screen name="Highlight" component={DemoBox} />
+      <Drawer.Screen name="Tooltip" component={DemoBox} />
+      <Drawer.Screen name="Modal" component={DemoBox} />
+      <Drawer.Screen name="Popover" component={DemoBox} />
+      <Drawer.Screen name="Accordion" component={DemoBox} />
+      <Drawer.Screen name="Tabs" component={DemoBox} />
+      <Drawer.Screen name="Carousel" component={DemoBox} />
+      <Drawer.Screen name="Steps" component={DemoBox} />
     </Drawer.Navigator>
   );
 };
